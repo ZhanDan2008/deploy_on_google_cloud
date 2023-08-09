de9011b019be411d35cf43e1b46371c7eb4f6cc7
dsadsadsa2245 <daniel.zhanbolotov@uwis.edu.kg>
2023-08-09 13:37:51 +0600

    Создан файл runserver.sh

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = True
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('NAME'),
         'USER': os.environ.get('USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
---
+++ runserver.sh
@@ -0,0 +1,3 @@
+python3 manage.py migrate --settings=app.settings.prod
+
+python3 manage.py createsuperuser --settings=app.settings.prod
