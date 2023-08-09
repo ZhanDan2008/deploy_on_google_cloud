02a2288e47b07caa290dd331017684b130c58d33
dsadsadsa2245 <daniel.zhanbolotov@uwis.edu.kg>
2023-08-09 12:50:14 +0600

    Закомментил Database в base.py

--- app/settings/base.py
+++ app/settings/base.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('NAME'),
-        'USER': os.environ.get('USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('NAME'),
+#         'USER': os.environ.get('USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
 
 AUTH_USER_MODEL = 'account.CustomUser'
 REST_FRAMEWORK = {
